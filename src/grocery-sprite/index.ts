409a95c46f9ba74ed1000ed357ed538102748bb6
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-02 04:26:48 +0530

    Assemble grocery category WebP sprite

---
+++ src/grocery-sprite/index.ts
@@ -0,0 +1,12 @@
+import part0 from './part0';
+import part1 from './part1';
+import part2 from './part2';
+import part3 from './part3';
+import part4 from './part4';
+import part5 from './part5';
+import part6 from './part6';
+import part7 from './part7';
+
+const groceryKitchenSprite = `data:image/webp;base64,${part0}${part1}${part2}${part3}${part4}${part5}${part6}${part7}`;
+
+export default groceryKitchenSprite;
