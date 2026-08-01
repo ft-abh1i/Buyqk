603a7797af1621b8e1dd034eb28b71a66d5fb7e3
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-02 04:06:42 +0530

    Connect search page to homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { installSearchPage } from './search-page';
 import './styles.css';
 import './header-location.css';
 import './search-bar.css';
+import './search-page.css';
 import './category-cards.css';
 import './bottom-nav.css';
 import './promo-image';
@@ -13,3 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 );
+
+installSearchPage();
