75c023a6f458c5548c4ffc11f79d6d9aaa290b2b
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-02 03:29:59 +0530

    Load category artwork fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './category-cards.css';
 import './bottom-nav.css';
 import './promo-image';
 
