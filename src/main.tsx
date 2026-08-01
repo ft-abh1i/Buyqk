7dfea1171f4602284ecfe521dd3b62b3522c3cfd
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-02 04:53:56 +0530

    Enable compare card promo slider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { installSearchPage } from './search-page';
 import { installFoodJunctionImage } from './store-images/installFoodJunctionImage';
+import { installPromoCarousel } from './promo-carousel';
 import './styles.css';
 import './header-location.css';
 import './search-bar.css';
 import './search-page.css';
 import './category-cards.css';
+import './promo-carousel.css';
 import './store-images/freshmart.css';
 import './store-images/quickfix.css';
 import './bottom-nav.css';
@@ -22,3 +24,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
 installSearchPage();
 installFoodJunctionImage();
+installPromoCarousel();
