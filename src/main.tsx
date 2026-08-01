5c7220f4e5edd819763b7103a325ba930863005f
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-02 03:19:46 +0530

    Use compressed WebP image for local shops promo card

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './bottom-nav.css';
+import './promo-image';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
